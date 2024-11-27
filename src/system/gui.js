464c90727128e41c1b919194d23766392ea9132e
asoutherncat <lysunny0302@163.com>
2024-11-27 23:29:42 +0800

    style: set tweakpane theme

--- src/system/gui.js
+++ src/system/gui.js
@@ -1,7 +1,8 @@
 // import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
 import { Pane } from 'tweakpane';
 
-const pane = new Pane();
+const pane = new Pane({ title: 'Amazing-Globe' });
+pane.expanded = false;
 // const gui = new GUI();
 // const debugObject = {};
 
